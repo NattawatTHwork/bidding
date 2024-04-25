eb9df561a8cb9268e63bcf9583933e098ad53da0
NattawatTHwork <nattawatth.work@gmail.com>
2024-04-25 12:14:16 +0700

    start

--- connect/connect.php
+++ connect/connect.php
@@ -10,7 +10,7 @@ try {
     // If you change db server system, change this too!
     $connect = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user,$db_pass );
     $connect->exec("set names utf8mb4");
-    // echo "Connected to database";
+    echo "Connected to database";
 }
 catch (PDOException $e) {
     echo $e->getMessage();
